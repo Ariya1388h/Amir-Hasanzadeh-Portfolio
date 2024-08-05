7e4c45fa8fe8d2cc013a2cf51647e3baca2bf049
Ariya1388h <aria.hasanzade8@gmail.com>
2024-08-05 19:39:25 +0330

    hmmm

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+const tl = gsap.timeline()
+
+tl 
+    .to(['.social-icons a','.btn-group a'], {
+        x: 0,
+        stagger: .2,
+        duration: .3,
+        ease: "power4.inOut",
+    })
+    // .to('.btn-group a', {
+    //     x: 0,
+    //     stagger: .2,
+    //     duration: .3,
+    //     ease: "power4.inOut",
+    // })
 
 // Home
 
