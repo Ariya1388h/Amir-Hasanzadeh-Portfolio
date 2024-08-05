253aa0902ef1452e24498d64764845f064d19dfc
Ariya1388h <aria.hasanzade8@gmail.com>
2024-08-05 19:55:53 +0330

    fix css

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ menuIcon.onclick = () => {
 const tl = gsap.timeline()
 
 tl 
-    .to(['.social-icons a','.btn-group a'], {
+    .to('.social-icons a', {
         x: 0,
-        stagger: .2,
-        duration: .3,
+        stagger: -0.2,
+        duration: .5,
         ease: "power4.inOut",
     })
-    // .to('.btn-group a', {
-    //     x: 0,
-    //     stagger: .2,
-    //     duration: .3,
-    //     ease: "power4.inOut",
-    // })
+    .to('.btn-group a', {
+        stagger: .2,
+        duration: .5,
+        opacity: '100',
+    })
 
 // Home
 
