d917a2cd1832821d85b9a925076e6021495034ce
Ariya1388h <aria.hasanzade8@gmail.com>
2024-08-09 16:59:11 +0330

    some fixed

--- script.js
+++ script.js
@@ -9,17 +9,48 @@ menuIcon.onclick = () => {
 const tl = gsap.timeline()
 
 tl 
-    .to('.social-icons a', {
+    .to('.home-social-icons a', {
         x: 0,
         stagger: -0.2,
         duration: .5,
         ease: "power4.inOut",
+        scrollTrigger: {
+            trigger: '.home-social-icons a',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+          },
     })
+
     .to('.btn-group a', {
         stagger: .2,
         duration: .5,
         opacity: '100',
+        scrollTrigger: {
+            trigger: '.btn-group a',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+        },
     })
+
+    .to('.about-content a', {
+        stagger: .2,
+        duration: .5,
+        opacity: '100',
+        boxShadow: '0 0 25px var(--main-color)',  
+        scrollTrigger: {
+            trigger: '.about-content a',
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+          },  
+    })
+
+    // .to('.home-img img', {
+    //     duration: 2,
+    //     clipPath: 'circle(50%)'
+    // })
+
+    
+
 
 // Home
 
