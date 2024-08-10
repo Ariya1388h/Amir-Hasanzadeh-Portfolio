cc6e50043066b378144e83b4e9ec362d47a41cca
Ariya1388h <aria.hasanzade8@gmail.com>
2024-08-10 15:35:03 +0330

    scrub: true,

--- script.js
+++ script.js
@@ -9,39 +9,21 @@ menuIcon.onclick = () => {
 const tl = gsap.timeline()
 
 tl 
+
+
+    // Home
+
     .to('.home-social-icons a', {
         x: 0,
         stagger: -0.2,
         duration: .5,
         ease: "power4.inOut",
-        scrollTrigger: {
-            trigger: '.home-social-icons a',
-            start: 'top 80%',
-            toggleActions: 'play none none none',
-          },
     })
 
     .to('.btn-group a', {
         stagger: .2,
         duration: .5,
         opacity: '100',
-        scrollTrigger: {
-            trigger: '.btn-group a',
-            start: 'top 80%',
-            toggleActions: 'play none none none',
-        },
-    })
-
-    .to('.about-content a', {
-        stagger: .2,
-        duration: .5,
-        opacity: '100',
-        boxShadow: '0 0 25px var(--main-color)',  
-        scrollTrigger: {
-            trigger: '.about-content a',
-            start: 'top 80%',
-            toggleActions: 'play none none none',
-          },  
     })
 
     // .to('.home-img img', {
@@ -49,13 +31,37 @@ tl
     //     clipPath: 'circle(50%)'
     // })
 
+
+    // About
+
+    .to('.about-content a', {
+        stagger: .2,
+        opacity: '100',
+        boxShadow: '0 0 25px var(--main-color)',  
+        scrollTrigger: {
+            trigger: '.about-content a',
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+            toggleActions: 'play none play none',
+          },  
+    })
+
     
+    // Services
 
+    .to('.service-box', {
+        height: 400,
+        backgroundColor: '#0077ff',
+        color: 'black',
+        ease: "power4.inOut",
+        scrollTrigger: {
+            trigger: '.service-box',
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+            toggleActions: 'play none play none',
+        },  
+    })
 
-// Home
-
-// About
-
-// Services
-
-// Contact
+    
